bb31091e77efa638ecbcf1dc4c9c0e7183a75e67
Aleksandr Kashin <alexkashin.90@gmail.com>
2021-06-22 13:35:06 +0300

    Changed deprecated method

--- src/test/java/ru/training/at/hw3/utils/DriverManager.java
+++ src/test/java/ru/training/at/hw3/utils/DriverManager.java
@@ -1,6 +1,8 @@
 package ru.training.at.hw3.utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +12,7 @@ public class DriverManager {
     public static WebDriver createDriver() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.manage().window().maximize();
         return driver;
     }
